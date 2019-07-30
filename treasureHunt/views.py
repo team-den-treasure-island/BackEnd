e0da981c5775a35699484a5681314431b6363543
Kory Newton <korybnewton@gmail.com>
2019-07-29 20:40:54 -0700

    player views, CRUD, update readme

--- treasureHunt/views.py
+++ treasureHunt/views.py
@@ -53,3 +53,46 @@ class RoomDetailsView(APIView):
             serialize.save()
             return Response(serialize.data)
         return Response(serialize.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PlayerDetailsView(APIView):
+    def get_object(self, pk):
+        try:
+            return Player.objects.get(pk=pk)
+
+        except Player.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        room = self.get_object(pk)
+        serialize = PlayerSerializer(room)
+
+        return Response(serialize.data)
+
+    def post(self, request, format=None):
+        serialize = PlayerSerializer(data=request.data)
+        if serialize.is_valid():
+            serialize.save()
+            return Response(serialize.data, status=status.HTTP_201_CREATED)
+        return Response(serialize.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk, format=None):
+        player = self.get_object(pk)
+        serialize = PlayerSerializer(player, data=request.data)
+        if serialize.is_valid():
+            serialize.save()
+            return Response(serialize.data)
+        return Response(serialize.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        player = self.get_object(pk)
+        player.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request, pk, format=None):
+        player = self.get_object(pk)
+        serialize = PlayerSerializer(player, data=request.data, partial=True)
+        if serialize.is_valid():
+            serialize.save()
+            return Response(serialize.data)
+        return Response(serialize.errors, status=status.HTTP_400_BAD_REQUEST)
