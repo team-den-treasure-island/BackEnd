6dd0ed94aa1d91741cc0ce7a97dc3f80a6f33230
Kory Newton <korybnewton@gmail.com>
2019-07-29 19:25:30 -0700

    keys abstracted into .env file

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+# DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
