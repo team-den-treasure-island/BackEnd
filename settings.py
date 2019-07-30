422fc7b93d06b38ea9ce43c57acec64e7658f4a4
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-07-30 15:18:22 -0400

    add heroku guide files

--- settings.py
+++ settings.py
@@ -7,3 +7,14 @@ load_dotenv(dotenv_path)
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+
+
+
+
+
+
+# HEROKU STUFF
+# https://github.com/heroku/django-heroku
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
