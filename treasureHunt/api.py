e2206d6ce18cb984be4327e526a0b485a216a81d
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-07-31 09:57:46 -0400

    add token authentication
    this change combines token and session authentication for all endpoints,
    users will need to be authenticated via one or the other

--- treasureHunt/api.py
+++ treasureHunt/api.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers, viewsets
 from .models import Room, Player
 from collections import OrderedDict
+from rest_framework.permissions import BasePermission, IsAuthenticatedOrReadOnly
+from rest_framework.authentication import TokenAuthentication
+
 
 # convention is to name the serializer classes after what they
 # are serializing
 
+
+# class Token
 
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
     # lookup_field = 'room_id'
@@ -32,6 +37,9 @@ class RoomSerializer(serializers.HyperlinkedModelSerializer):
 
 # has access to request directly
 class RoomViewSet(viewsets.ModelViewSet):
+    # can bitwise operate on these permission classes
+    # want permission to be either token or session
+    # permission_classes = [TokenAuthentication | IsAuthenticatedOrReadOnly]
     serializer_class = RoomSerializer
     queryset = Room.objects.none()  # empty dictionary
     lookup_field = "room_id"
--- treasureHuntBE/urls.py
+++ treasureHuntBE/urls.py
@@ -15,9 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import rest_framework
 from rest_framework import routers
 from treasureHunt.api import RoomViewSet, PlayerViewSet
 from graphene_django.views import GraphQLView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import authentication_classes, permission_classes, api_view
+from rest_framework.settings import api_settings
+
+# solution to use rest_framework to guard graphql
+# https://github.com/graphql-python/graphene/issues/249#issuecomment-300068390
+# this even uses our main settings.py configuration for authentication and permissions
+# so it will be accessible via session or token
+class DRFAuthenticatedGraphQLView(GraphQLView):
+    def parse_body(self, request):
+        if isinstance(request, rest_framework.request.Request):
+            return request.data
+        return super(GraphQLView, self).parse_body(request)
+
+    @classmethod
+    def as_view(cls, *args, **kwargs):
+        view = super(GraphQLView, cls).as_view(*args, **kwargs)
+        view = permission_classes((IsAuthenticated,))(view)
+        view = authentication_classes(api_settings.DEFAULT_AUTHENTICATION_CLASSES)(view)
+        view = api_view(['GET', 'POST'])(view)
+        return view
 
 
 router = routers.DefaultRouter()
@@ -30,8 +52,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     # add graphQL route with graphiql interface enabled
-    path("graphql/", GraphQLView.as_view(graphiql=True)),
+    # path("graphql/", GraphQLView.as_view(graphiql=True)),
+    path("graphql/", DRFAuthenticatedGraphQLView.as_view(graphiql=True)),
 ]
 
   # if we need custom routes, we can add this back
   # path('', include('treasureHunt.urls')),
+
+# authenticate graphql maybe
+
